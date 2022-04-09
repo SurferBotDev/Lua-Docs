d447d9f0740fdd5ebabc83d76d36584332df47d7
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2022-04-09 11:12:43 +0300

    Delete dropallitem.lua

--- Scripts/dropallitem.lua
+++
@@ -1,5 +0,0 @@
-for _,cur in pairs(getInventory()) do
-sendPacket(false,"action|drop\n|itemID|"..cur.id,2)
-sendPacket(false,"action|dialog_return\ndialog_name|drop_item\nitemID|"..cur.id.."|\ncount|"..cur.count,2)
-Sleep(150)
-end
