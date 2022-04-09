a62bf7c59a9315374c4080f092a817f6c85c4915
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2022-04-09 13:17:11 +0300

    Update dropallitem.lua

--- Scripts/dropallitem.lua
+++ Scripts/dropallitem.lua
@@ -1,5 +1,7 @@
-for _,cur in pairs(getInventory()) do
-sendPacket("action|drop\n|itemID|"..cur.id,2)
-sendPacket("action|dialog_return\ndialog_name|drop_item\nitemID|"..cur.id.."|\ncount|"..cur.count,2)
+bot = getBot("growid")
+
+for _,cur in pairs(bot:getInventory()) do
+bot:sendPacket("action|drop\n|itemID|"..cur.id,2)
+bot:sendPacket("action|dialog_return\ndialog_name|drop_item\nitemID|"..cur.id.."|\ncount|"..cur.count,2)
 Sleep(150)
 end
