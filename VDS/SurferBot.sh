03c799ce094afd7305b2fad1e6ba1fa36cce9564
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2022-04-19 14:35:42 +0300

    Update SurferBot.sh

--- VDS/SurferBot.sh
+++ VDS/SurferBot.sh
@@ -1,5 +1,5 @@
 killall -9 SurferBot
 unlink SurferBot
-wget https://cdn.discordapp.com/attachments/896160034919174145/962409364642218035/SurferBot
+wget https://cdn.discordapp.com/attachments/896160034919174145/965938573868138566/SurferBot
 chmod 777 SurferBot
 ./SurferBot
